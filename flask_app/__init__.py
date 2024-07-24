4acf50a58fc55075d4bcc95d297660ddce8d5d9e
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-07-24 22:34:25 +0100

    full support for custom guis

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,10 +1,10 @@
 import importlib.util
 
-from flask import Flask
+from flask import Flask, Blueprint, render_template
 
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 
 from flask_app import ws_routes
@@ -13,11 +13,32 @@ from flask_app import views
 
 def load_logic_module(module_path):
     spec = importlib.util.spec_from_file_location("logic", module_path)
-    logic = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(logic)
-    return logic.Logic()
+    logic_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(logic_module)
+    return logic_module.Logic()
 
 
 logic = load_logic_module(app.config["LOGIC_MODULE"])
 
 app.logic = logic
+
+
+if app.logic.configs.get("custom_template", False):
+    custom_dir = app.logic.configs["custom_template"]
+    custom_bp = Blueprint(
+        'custom',
+        __name__,
+        static_folder=f"{custom_dir}/static",
+        template_folder=f"{custom_dir}/templates",
+        url_prefix="/custom"
+    )
+
+
+    @custom_bp.route('/')
+    def custom_index():
+        title = app.logic.configs.get("title", "Generic LAN Python GUI")
+
+        return render_template("custom.html", title=title)
+
+    app.register_blueprint(custom_bp)
+    app.config["CUSTOM_PAGE"] = True
--- flask_app/views.py
+++ flask_app/views.py
@@ -1,9 +1,11 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask_app import app
 
 
 @app.route("/")
 def index():
 	title = app.logic.configs.get("title", "Generic LAN Python GUI")
-
-	return render_template("index.html", title=title)
+	if app.config.get("CUSTOM_PAGE", False):
+		return redirect(url_for("custom.custom_index"))
+	else:
+		return render_template("index.html", title=title)
